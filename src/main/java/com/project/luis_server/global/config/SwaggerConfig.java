6a689c1f308e597c55c6609270fcd9752878d0e4
lgwk42 <luisgunkim@gmail.com>
2024-06-12 11:53:34 +0900

    feat :: config directory add

---
+++ src/main/java/com/project/luis_server/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.project.luis_server.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** swagger 설정 파일*/
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Luis")
+                .description("Luis API Document")
+                .version("0.0.1");
+    }
+
+}
