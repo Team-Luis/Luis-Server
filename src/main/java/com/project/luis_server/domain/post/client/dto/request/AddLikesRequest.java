dbb2416f11ac2b49eebf8524d31a67fe93d44af8
lgwk42 <luisgunkim@gmail.com>
2024-06-18 14:44:57 +0900

    fix :: add like req add

---
+++ src/main/java/com/project/luis_server/domain/post/client/dto/request/AddLikesRequest.java
@@ -0,0 +1,12 @@
+package com.project.luis_server.domain.post.client.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddLikesRequest {
+
+    private Long postId;
+
+}
