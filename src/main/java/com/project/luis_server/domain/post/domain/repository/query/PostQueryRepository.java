4d659cbe8cdea05d2d23df1bdad14cffcf3f3a64
lgwk42 <luisgunkim@gmail.com>
2024-06-18 14:53:58 +0900

    feat :: post search query add

--- src/main/java/com/project/luis_server/domain/post/domain/repository/query/PostQueryRepository.java
+++ src/main/java/com/project/luis_server/domain/post/domain/repository/query/PostQueryRepository.java
@@ -1,10 +1,13 @@
 package com.project.luis_server.domain.post.domain.repository.query;
 
 import com.project.luis_server.domain.post.client.dto.Post;
+import com.project.luis_server.domain.post.client.dto.request.PostSearchRequest;
 import com.project.luis_server.global.common.dto.request.PageRequest;
 
 import java.util.List;
 
 public interface PostQueryRepository {
     List<Post> postList(PageRequest request);
+
+    List<Post> postSearch(PostSearchRequest request);
 }
--- src/main/java/com/project/luis_server/domain/post/domain/repository/query/PostQueryRepositoryImpl.java
+++ src/main/java/com/project/luis_server/domain/post/domain/repository/query/PostQueryRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.project.luis_server.domain.post.domain.repository.query;
 
 import com.project.luis_server.domain.post.client.dto.Post;
+import com.project.luis_server.domain.post.client.dto.request.PostSearchRequest;
 import com.project.luis_server.global.common.dto.request.PageRequest;
+import com.project.luis_server.global.exception.BadRequestException;
 import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -29,13 +32,33 @@ public class PostQueryRepositoryImpl implements PostQueryRepository {
                 .fetch();
     }
 
+    @Override
+    public List<Post> postSearch(PostSearchRequest request){
+        return jpaQueryFactory
+                .select(postListConstructorExpression())
+                .from(postEntity)
+                .where(eqTitle(request.getTitle()))
+                .offset((request.getPage() - 1) * request.getSize())
+                .limit(request.getSize())
+                .orderBy(postEntity.idx.asc())
+                .fetch();
+    }
+
     private ConstructorExpression<Post> postListConstructorExpression(){
         return Projections.constructor(Post.class,
                 postEntity.idx,
                 postEntity.title,
                 postEntity.content,
-                postEntity.author
+                postEntity.author,
+                postEntity.likes
         );
+    }
+
+    private BooleanExpression eqTitle(String title) {
+        if(title.isEmpty()){
+            throw BadRequestException.EXCEPTION;
+        }
+        return postEntity.title.eq(title);
     }
 
 }
