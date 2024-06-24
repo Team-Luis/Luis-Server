3174146fd472ecd24025b7a99a01ba0da6b8a9e0
lgwk42 <luisgunkim@gmail.com>
2024-06-24 20:43:12 +0900

    fix :: add transactional in post repo

--- src/main/java/com/project/luis_server/domain/post/domain/repository/jpa/PostRepository.java
+++ src/main/java/com/project/luis_server/domain/post/domain/repository/jpa/PostRepository.java
@@ -1,12 +1,14 @@
 package com.project.luis_server.domain.post.domain.repository.jpa;
 
 import com.project.luis_server.domain.post.domain.PostEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
+    @Transactional(rollbackOn = Exception.class)
     void deleteByIdx(Long idx);
 
     Optional<PostEntity> findByIdx(Long idx);
